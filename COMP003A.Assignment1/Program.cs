0bf540e143c74ca2cd2b95e5fd82e57995ac4771
Jean Figeuroa <jbster559@gmail.com>
2025-01-16 13:52:33 -0800

    Added to list

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -16,7 +16,8 @@ namespace COMP003A.Assignment1
             string userName = Console.ReadLine();
             Console.WriteLine("Hello, " + userName + "! My name is Jean");
             Console.WriteLine("Here are 10 things about me");
-            Console.WriteLine("\t1. I am a CS major at Lemoore College\n\t2. Test\n\t3.test\n\t4. test\n\t5. Test\n\t6.test\n\t7. test\n\t8. Test\n\t9.test\n\t10. Test");
+            Console.WriteLine("\t1. I am a CS major at Lemoore College\n\t2. I am slowly working on a videogame\n\t3. I've been investing in computers since a young age\n\t4. This is not my first time using C#\n\t5. I drive a Prius. It feels liberating to go over 60mph because it takes a 30 secconds to go that fast\n\t6. I enjoy a plethora of videogames\n\t7. I like a lot of rock\n\t8. Test\n\t9. test\n\t10. I am a brother of 4 (5 in a few months)");
+
         }
     }
 }
