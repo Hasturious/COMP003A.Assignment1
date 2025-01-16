32ca57e525f11f6bff9ca5ecadc2693d2410687b
Jean Figeuroa <jbster559@gmail.com>
2025-01-16 13:16:09 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Author: Jean Bryant Figueroa
+ * Course: COMP003A C#
+ * Faculty: Johnathan Cruz
+ * Purpose: Assignment 1 - 10 things about me
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
