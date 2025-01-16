7ee833fdf46a7f9d11cc4ce46d82d03f85ed988a
Jean Figeuroa <jbster559@gmail.com>
2025-01-16 13:35:33 -0800

    Got the writeline to read and write the name

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -3,6 +3,7 @@
  * Course: COMP003A C#
  * Faculty: Johnathan Cruz
  * Purpose: Assignment 1 - 10 things about me
+ */
 
 namespace COMP003A.Assignment1
 {
@@ -10,7 +11,12 @@ namespace COMP003A.Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello, " + userName + "! My name is Jean");
+            Console.WriteLine("Here are 10 things about me");
+            Console.WriteLine("\t1. I am a CS major at Lemoore College\n\t2. Test\n\t3.test\n\t4. test\n\t5. Test\n\t6.test\n\t7. test\n\t8. Test\n\t9.test\n\t10. Test");
         }
     }
 }
