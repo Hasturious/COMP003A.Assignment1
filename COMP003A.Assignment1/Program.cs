d4382a6f7156763c6859d94205a39bc4943aaea0
Jean Figeuroa <jbster559@gmail.com>
2025-01-17 13:58:17 -0800

    Finished List

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -16,7 +16,7 @@ namespace COMP003A.Assignment1
             string userName = Console.ReadLine();
             Console.WriteLine("Hello, " + userName + "! My name is Jean");
             Console.WriteLine("Here are 10 things about me");
-            Console.WriteLine("\t1. I am a CS major at Lemoore College\n\t2. I am slowly working on a videogame\n\t3. I've been investing in computers since a young age\n\t4. This is not my first time using C#\n\t5. I drive a Prius. It feels liberating to go over 60mph because it takes a 30 secconds to go that fast\n\t6. I enjoy a plethora of videogames\n\t7. I like a lot of rock\n\t8. Test\n\t9. test\n\t10. I am a brother of 4 (5 in a few months)");
+            Console.WriteLine("\t1. I am a CS major at Lemoore College\n\t2. I am slowly working on a videogame\n\t3. I've been interested in computers since a young age\n\t4. This is not my first time using C#\n\t5. I drive a Prius. It feels liberating to go over 60mph because it takes a 30 secconds to go that fast\n\t6. I enjoy a plethora of videogames\n\t7. I like a lot of rock\n\t8. I have the JetBrains IDE\n\t9. Firefox is my prefered browser\n\t10. I am a brother of 4 (5 in a few months)");
 
         }
     }
